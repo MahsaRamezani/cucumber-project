6d46a897eee19d264060d7635414a4da097995cf
m.ramezani <m.ramezani@sadad.co.ir>
2023-05-22 17:02:58 +0430

    persian-language

--- src/test/java/com/example/cucumberTest/runners/CucumberRunnerTest.java
+++ src/test/java/com/example/cucumberTest/runners/CucumberRunnerTest.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber-reports/Cucumber.html, json:target/cucumber-reports/Cucumber.json, junit:target/cucumber-reports/Cucumber.xml"},
+    publish = true,
+    plugin = {"pretty", "json:target/cucumber-report.json", "html:target/cucumber-reports/Cucumber.html, json:target/cucumber-reports/Cucumber.json, junit:target/cucumber-reports/Cucumber.xml"},
     glue = {"com.example.cucumberTest.stepDefinitions"},
 //    tags = "@datadriven and not @bug",
     features = "classpath:features"
