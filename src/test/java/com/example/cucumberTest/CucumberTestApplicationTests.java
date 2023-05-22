e30775bac9438bb949d1cb500e6e447b035b7104
m.ramezani <m.ramezani@sadad.co.ir>
2023-05-22 09:40:16 +0430

    initial-commit

---
+++ src/test/java/com/example/cucumberTest/CucumberTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cucumberTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
